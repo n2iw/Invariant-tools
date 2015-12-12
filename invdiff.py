0d6eea03edc107764434c7f85088ef1c4627061c
n2iw <yingming18@gmail.com>
2015-12-11 21:23:40 -0500

    Finish first attempt on compareInv()

--- invdiff.py
+++ invdiff.py
@@ -22,11 +22,12 @@ def loadInvariant(fileName):
             if new_ppt:
                 new_ppt = False
                 ppt = line.rstrip("\n\r")
-                data[ppt] = []
+                data[ppt] = set() 
             else:
-                data[ppt].append(line.rstrip("\n\r"))
+                data[ppt].add(line.rstrip("\n\r"))
     f.close()
     return data
+
 def printInv(data):
     print 'Invariant file has ' + str(len(data.keys())) + ' Program Points'
     for ppt in data:
@@ -35,8 +36,30 @@ def printInv(data):
         for inv in data[ppt]:
             print '  ' + inv
 
-def compareInv(inv1, inv2):
+# data1 is the invariant file for bug-free version 
+# data2 is the invariant file for buggy version
+# only compare Program Points that exits in both data1 and data2
+def compareInv(data1, data2):
+    data1_only = {}
+    data2_only = {}
+    both = {}
     print 'Comparing invariants'
+    for ppt in data2:
+        if ppt in data1:
+            intersect = data1[ppt] & data2[ppt]
+            if intersect:
+                both[ppt] = intersect
+
+            d1_only = data1[ppt] - data2[ppt]
+            if d1_only:
+                data1_only[ppt] = d1_only
+
+            d2_only = data2[ppt] - data1[ppt]
+            if d2_only:
+                data2_only[ppt] = d2_only
+        elif data2[ppt]:
+            data2_only[ppt] = data2[ppt]
+    return (data1_only, data2_only, both)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
@@ -49,9 +72,19 @@ if __name__ == '__main__':
         print sys.argv[2] + ' is not a file!'
         exit(1)
     inv1 = loadInvariant(sys.argv[1])
-    printInv(inv1)
+    #printInv(inv1)
     inv2 = loadInvariant(sys.argv[2])
-    #print '========================='
+    #print ''
     #printInv(inv2)
     
-    #diff = compareInv(inv1, inv2)
+    (d1_only, d2_only, both) = compareInv(inv1, inv2)
+    print ''
+    print 'Invariants only in ' + sys.argv[1]
+    printInv(d1_only)
+    print ''
+    print 'Invariants only in ' + sys.argv[2]
+    printInv(d2_only) 
+    print ''
+    print 'Invariants in both files'
+    printInv(both)
+
