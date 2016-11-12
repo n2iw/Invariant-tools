cde8ecbfb068b152b4b2b17985ce09581a28bc22
Ming Ying <yingming18@gmail.com>
2016-11-11 23:32:48 -0500

    Add a new method to do validation, which use Adil's number defination

--- validate.py
+++ validate.py
@@ -10,17 +10,24 @@ import csv
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Validates a set of invariant numbers against a csv file")
-    parser.add_argument('csv', help='csv file to Validate', type=argparse.FileType('r'))
+    parser.add_argument('csv', help='csv file to Validate', type=argparse.FileType('rb'))
     parser.add_argument('invs', nargs='+', help='invariant numbers')
+    parser.add_argument('-a', '--adil', action='store_true', help="use csv line number as invariant number" )
     args = parser.parse_args()
 
     invs = set()
     for inv in args.invs:
-        invs.add(inv)
+        invs.add(int(inv))
 
     reader = csv.reader(args.csv)
     writer = csv.writer(sys.stdout)
 
+    lineNum = -1
     for row in reader:
-        if row[0] in invs:
-            writer.writerow(row)
+        if args.adil:
+            if lineNum in invs:
+                writer.writerow(row)
+        else:
+            if int(row[0]) in invs:
+                writer.writerow(row)
+        lineNum += 1
