e81c53a27e38e52fe2448705f0576f97f0636b13
Ming Ying <yingming18@gmail.com>
2017-01-22 16:11:59 -0500

    Add quiet mode, default to Adil mode

--- validate.py
+++ validate.py
@@ -25,9 +25,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Validates a set of invariant numbers against a csv file")
     parser.add_argument('csv', help='csv file to Validate', type=argparse.FileType('r'))
     parser.add_argument('invs', nargs='+', help='invariant numbers')
-    parser.add_argument('-a', '--adil', action='store_true', help="use csv line number as invariant number" )
+    parser.add_argument('-a', '--adil', action='store_true', help="use Adil's way: csv line number as invariant number", default=True)
+    parser.add_argument('-m', '--me', action='store_true', help="use my way: invariant number (first column in csv file)")
+    parser.add_argument('-q', '--quiet', action='store_true', help="only output number of bugs detected")
     parser.add_argument('-n', '--no-names', action='store_true', help="instead of printing program point and invariant names, print line numbers, this option can significantly reduce output file size")
     args = parser.parse_args()
+    if args.me:
+        # print("Me")
+        args.adil = False
+    # if args.adil:
+        # print("Adil")
 
     invs = set()
     for inv in args.invs:
@@ -47,21 +54,30 @@ if __name__ == '__main__':
     for header in headers[startCol:]:
         sums.append(0)
 
-    writer.writerow(headers)
+    if not args.quiet:
+        writer.writerow(headers)
     lineNum = 0
     for row in reader:
         if args.adil:
             if lineNum in invs:
                 addRowToSum(sums, row, startCol)
-                writer.writerow(row)
+                if not args.quiet:
+                    writer.writerow(row)
                 invs.remove(lineNum)
                 if len(invs) == 0:
                     break
-        else:
+        elif args.me:
             if int(row[0]) in invs:
                 addRowToSum(sums, row, startCol)
-                writer.writerow(row)
+                if not args.quiet:
+                    writer.writerow(row)
+                invs.remove(int(row[0]))
+                if len(invs) == 0:
+                    break
         lineNum += 1
 
     sums.append(totalDetected(sums, startCol))
-    writer.writerow(sums)
+    if not args.quiet:
+        writer.writerow(sums)
+    else:
+        writer.writerow(sums[-1:])
